70af3e0415f7852ad12ade352dd56fca8764c647
Anitha <S545502@nwmissouri.edu>
2022-01-25 18:17:30 -0600

    Initial Commit

---
+++ Week1.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("Hii",10,12.25)
+
+var programmingLanguage = "Swift"
+print("My favorite programming language is \(programmingLanguage)")
+
+var age = 23
+print("You are \(age) years old and in another \(age) years, you will be \(age * 2)")
+
+print()
